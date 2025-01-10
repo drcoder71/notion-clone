9f63aaa815db8568dfe3a26c3f3368f98b2001ec
drCoder <drcoderjs@gmail.com>
2025-01-10 07:09:47 +0500

    convex schema

---
+++ convex/document.ts
@@ -0,0 +1,24 @@
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createDocs = mutation({
+  args: {
+    title: v.string(),
+    userId: v.string(),
+    isArchive: v.boolean(),
+    isPublished: v.boolean(),
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const indentity = await ctx.auth.getUserIdentity();
+    if (!indentity) throw new Error("You're not authentificated");
+    const userId = indentity.subject;
+    const documents = await ctx.db.insert("documents", {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId,
+      isArchived: false,
+      isPublished: false,
+    });
+  },
+});
